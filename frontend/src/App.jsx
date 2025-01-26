106431f8d0d7865ea15253cf0b38bfc5f045d819
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2025-01-26 21:30:56 +0530

    set enviroment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Button>Click me</Button>
+  )
+}
+
+export default App
