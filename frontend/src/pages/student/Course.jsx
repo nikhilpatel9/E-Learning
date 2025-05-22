488f966711cfbea6889aef50923e5564b03f1cc0
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2025-05-22 11:41:41 +0530

    Update Course.jsx

--- frontend/src/pages/student/Course.jsx
+++ frontend/src/pages/student/Course.jsx
@@ -38,7 +38,7 @@ const Course = ({ course }) => {
             </Badge>
           </div>
           <div className="text-lg font-bold">
-            <span>₹{course.coursePrice}</span>
+            <span>${course.coursePrice}</span>
           </div>
         </CardContent>
       </Card>
